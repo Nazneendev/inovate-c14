2f374a496b69a1d2ac6daf5bb6b4c37a600fcd24
Nazneen Akter <nazneenzaman195@gmail.com>
2025-01-10 00:19:17 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,39 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // color 
+        colors: {
+          headerBg: '#DBD5FB',
+          btnColor:'#7258F7',
+          h1color:'#3D3430',
+          bgColor:'#FCF8F8',
+        },
+         // color
+        // container 
+        maxWidth: {
+        'headerContainer': '1170px',
+      },
+      // container 
+      // Font Family 
+      fontFamily: {
+      
+      'open': ["Open Sans"],
+      'papri': ["Paprika"],
+      
+    },
+       // Font Family 
+        // backgroundImage
+       backgroundImage: {
+        bannerBg: "url('../images/bg-page-title7.png')",
+        bannerBgTwo:"url('../images/bg-page-title8.png')",
+       },
+        // backgroundImage
+        // font size 
+         // font size 
+        
+     
+    }
+       
+      }
+    }
+  
